d513b7dd0d87f694fc112cb83a18609a46279eaa
kevinr-onisep <kevin.rifa@onisep.fr>
2025-11-26 10:22:56 +0100

    Generate CLI

---
+++ grc_templates/README.md
@@ -0,0 +1,10 @@
+# composant react TemplateName
+
+## usage
+
+&lt;TemplateName /&gt;
+
+### props de templateName
+
+liste des propos :
+    - Aucune
---
+++ grc_templates/functionBasic/TemplateName.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './TemplateName.module.css';
+
+interface ITemplateNameProps {}
+
+const TemplateName: React.FC<ITemplateNameProps> = (props) => {
+  return(
+    <div className={styles.TemplateName} data-testid="TemplateName">
+      TemplateName Component
+    </div>
+  );
+}
+
+export default TemplateName;
