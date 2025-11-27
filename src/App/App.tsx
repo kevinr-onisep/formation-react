2edf76593d1892c77d7b2864d3ce0f28528b7f70
kevinr-onisep <kevin.rifa@onisep.fr>
2025-11-27 16:47:53 +0100

    Refacto code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,48 +1,21 @@
 import "./App.css";
-import "./stores/store";
-import MemeForm from "./components/functionnal/MemeForm/MemeForm";
 import FlexH1stGrow from "./components/layout/FlexH1stGrow/FlexH1stGrow";
 import FlexV3rdGrow from "./components/layout/FlexV3rdGrow/FlexV3rdGrow";
 import Header from "./components/ui/Header/Header";
 import Navbar from "./components/ui/Navbar/Navbar";
 import Footer from "./components/ui/Footer/Footer";
-import {
-  emptyMeme,
-  MemeSVGViewer,
-  type ImageInterface,
-  type MemeInterface,
-} from "orsys-tjs-meme";
-import { useEffect, useState } from "react";
-import { REST_ADR, REST_RESOURCES } from "./constantes/config";
+import MemeSVGViewer from "./components/ui/MemeSVGViewer/MemeSVGViewer.connected";
+import MemeForm from "./components/functionnal/MemeForm/MemeForm.connected";
 
 function App() {
-  const [current, setCurrent] = useState<MemeInterface>(emptyMeme);
-  const [images, setImages] = useState<Array<ImageInterface>>([]);
-
-  useEffect(() => {
-    fetch(`${REST_ADR}${REST_RESOURCES.images}`)
-      .then((r) => r.json())
-      .then((arr) => setImages(arr));
-  }, []);
-
   return (
     <div className="App">
       <FlexV3rdGrow>
         <Header />
         <Navbar />
         <FlexH1stGrow>
-          <MemeSVGViewer
-            meme={current}
-            image={images.find((img) => img.id === current.imageId)}
-            basePath=""
-          />
-          <MemeForm
-            meme={current}
-            onMemeChange={(meme) => {
-              setCurrent(meme);
-            }}
-            images={images}
-          />
+          <MemeSVGViewer />
+          <MemeForm />
         </FlexH1stGrow>
         <Footer />
       </FlexV3rdGrow>
--- src/App/stores/store.ts
+++ src/App/stores/store.ts
@@ -6,12 +6,6 @@ export const store = configureStore({
   reducer: { current: currentReducer, ressources: ressourcesReducer },
 });
 
-store.subscribe(() => {
-  console.log("%c%s", "font-size:24pt;color:tomato", "changement d'état");
-
-  console.log(store.getState());
-});
-
 store.dispatch(loadRessources());
 
 export type RootState = ReturnType<typeof store.getState>;
